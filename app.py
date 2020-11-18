9bdfa5e45b841d7c86e5dffc1dc21726061376fb
Pongsakorn Sommalai <bongtrop@gmail.com>
2020-11-18 23:28:09 +0700

    Fix issue static nonce

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import os
 
 FLAG = os.environ.get("FLAG") or "PLEASE_SET_A_FLAG"
 KEY = os.environ.get("KEY") or os.urandom(32)
-NONCE = os.environ.get("KEY") or os.urandom(15)
 
 app = Flask(__name__)
 
@@ -17,7 +16,7 @@ class Encryptor(object):
     def encrypt(self, plaintext):
         self._reset()
         key = AES.new(self.key, AES.MODE_CTR, counter=self.counter)
-        return key.encrypt(plaintext.encode()).hex()
+        return self.nonce.hex() + key.encrypt(plaintext.encode()).hex()
     
     def decrypt(self, ciphertext):
         self._reset()
@@ -27,7 +26,7 @@ class Encryptor(object):
     def _reset(self):
         self.counter = Counter.new(nbits=8, prefix=self.nonce, initial_value=0, little_endian=False, allow_wraparound=True)
 
-encryptor = Encryptor(KEY, NONCE)
+
 
 @app.route('/encrypt')
 def encrypt():
@@ -36,11 +35,16 @@ def encrypt():
     if not plaintext or len(plaintext) == 0:
         return "Something wents wrong"
     
+    nonce = os.urandom(15)
+    encryptor = Encryptor(KEY, nonce)
     return encryptor.encrypt(plaintext + FLAG)
 
 @app.route('/decrypt')
 def decrypt():
     ciphertext = request.args.get("ciphertext")
+    nonce = bytes.fromhex(ciphertext[:30])
+    ciphertext = ciphertext[30:]
+    encryptor = Encryptor(KEY, nonce)
     plaintext = encryptor.decrypt(ciphertext) 
     if not plaintext.endswith(FLAG):
         return "Something wents wrong"
