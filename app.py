e04ff0b20d083cc610c753c6bd5840c18a6c2b57
Pongsakorn Sommalai <bongtrop@gmail.com>
2020-11-24 20:09:45 +0700

    Fix unintented solution

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ app = Flask(__name__)
 class Encryptor(object):
     def __init__(self, key):
         self.key = key
-    
+
     def encrypt(self, plaintext):
         nonce = os.urandom(15)
         self._reset(nonce)
         key = AES.new(self.key, AES.MODE_CTR, counter=self.counter)
         return nonce.hex() + key.encrypt(plaintext.encode()).hex()
-    
+
     def decrypt(self, ciphertext):
         ciphertext = bytes.fromhex(ciphertext)
         self._reset(ciphertext[:15])
         key = AES.new(self.key, AES.MODE_CTR, counter=self.counter)
         return key.decrypt(ciphertext[15:]).decode()
-    
+
     def _reset(self, nonce):
         self.counter = Counter.new(nbits=8, prefix=nonce, initial_value=0, little_endian=False, allow_wraparound=True)
 
@@ -35,15 +35,15 @@ def encrypt():
 
     if not plaintext or len(plaintext) == 0:
         return "Something wents wrong"
-    
-    return encryptor.encrypt(plaintext + FLAG)
+
+    return encryptor.encrypt(FLAG + plaintext)
 
 @app.route('/decrypt', methods=['POST'])
 def decrypt():
     ciphertext = request.form.get('ciphertext')
 
-    plaintext = encryptor.decrypt(ciphertext) 
-    if not plaintext.endswith(FLAG):
+    plaintext = encryptor.decrypt(ciphertext)
+    if not plaintext.startswith(FLAG):
         return "Something wents wrong"
     else:
         return plaintext[:-len(FLAG)]
